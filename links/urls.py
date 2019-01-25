283fee1288823a39dc7b8de44d394a37bfed24a4
Jan Okruta <jan.okruta@gmail.com>
2019-01-25 20:28:35 +0100

    Add link detail to urls

--- links/urls.py
+++ links/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from links.views import add_link
+from links.views import add_link, link_detail
 
 urlpatterns = [
     path('', add_link),
+    path('<slug:slug>', link_detail, name='link_detail'),
 ]
