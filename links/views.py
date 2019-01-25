472dfba3445cd5317efae4cc16ff2587a5255d49
Jan Okruta <jan.okruta@gmail.com>
2019-01-25 20:15:36 +0100

    Create add link view

--- links/views.py
+++ links/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
-# Create your views here.
+from links.forms import LinkCreateForm
+
+
+def add_link(request):
+    if request.method == 'POST':
+        form = LinkCreateForm(request.POST)
+        if form.is_valid():
+            link_obj = form.save(commit=False)
+            link_obj.timestamp = timezone.now()
+            link_obj.save()
+            return redirect(link_obj.get_absolute_url())
+    else:
+        form = LinkCreateForm()
+        return render(request, 'add_link.html', {'form': form})
