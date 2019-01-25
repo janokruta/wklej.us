41fbeb25fb26671a9929b6332bc6c390c58d04b7
Jan Okruta <jan.okruta@gmail.com>
2019-01-25 19:59:50 +0100

    Link model definition

--- links/models.py
+++ links/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Link(models.Model):
+    slug = models.SlugField(blank=True, unique=True)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('link', kwargs={'slug': self.slug})
