3ab2d0b354d228abe1f812af98d02c94fda9a3eb
Jan Okruta <jan.okruta@gmail.com>
2019-01-25 20:09:01 +0100

    Auto-generate url for link

--- links/models.py
+++ links/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.dispatch import receiver
 from django.urls import reverse
+
+from links.utils import unique_slug_generator
 
 
 class Link(models.Model):
@@ -9,3 +12,9 @@ class Link(models.Model):
 
     def get_absolute_url(self):
         return reverse('link', kwargs={'slug': self.slug})
+
+
+@receiver(models.signals.pre_save, sender=Link)
+def add_slug(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
