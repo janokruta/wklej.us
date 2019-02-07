74a9d437d51b5f6f2e5369cc91306e2605355d25
Jan Okruta <jan.okruta@gmail.com>
2019-02-07 14:17:32 +0100

    URL generator changes

--- links/utils.py
+++ links/utils.py
@@ -2,12 +2,12 @@ from string import ascii_letters, digits
 from random import randint, choice
 
 
-def random_string_generator(size=8, chars=ascii_letters + digits):
+def random_string_generator(size, chars=ascii_letters[:26] + digits):
     return ''.join(choice(chars) for _ in range(size))
 
 
 def unique_slug_generator(instance):
-    slug = random_string_generator(size=randint(3, 6)).lower()
+    slug = random_string_generator(size=randint(3, 5))
     while instance.__class__.objects.filter(slug=slug).exists():
-        slug = random_string_generator(size=randint(3, 6)).lower()
+        slug = random_string_generator(size=randint(3, 5))
     return slug
